2eb167ed02f45b9034abdc5769d4ac43cfab99ff
Sumedha Kulkarni <sumedhakulkarni@Sumedhas-MacBook-Pro.local>
2022-06-11 21:28:19 +0530

    Migrated to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,45 @@
-import React, { useState, useEffect } from "react";
-import './App.css';
-
-type Props = {
-  textToType: string; delay: number; loop: boolean;
-}
-
-const desc = "Hi! I am Sumedha, a web developer based out of Bangalore, India."
-
-function TypingComponent({ textToType, delay, loop }: Props) {
-  const [text, setText] = useState("");
-  const [currentIndex, setCurrentIndex] = useState(0);
-  useEffect(() => {
-    if (currentIndex < textToType.length) {
-      setTimeout(() => {
-        setText(text + textToType[currentIndex]);
-        setCurrentIndex(currentIndex + 1);
-      }, delay);
-    }
-    else if (loop) {
-      setText("");
-      setCurrentIndex(0);
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentIndex]);
-  return <div>{text}<span className="blink">|</span></div>;
-}
-
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-
+  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <video width="100vw" height="100vh" autoPlay loop>
-        <source src="/background.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div className="content">
-        <div className="line app-content">
-          <TypingComponent textToType={desc} delay={100} loop={false} />
-        </div>
-      </div>
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
